97cc423c14ba72164f64d1ae80fafc7f0d1d2ad3
khalil <khalil.akbi@gmail.com>
2024-11-11 16:54:15 -0500

    we

--- src/main/java/pages/EmployerPage.java
+++ src/main/java/pages/EmployerPage.java
@@ -5,8 +5,9 @@ import base.Base;
 import org.openqa.selenium.support.PageFactory;
 
 public class EmployerPage extends Base {
-
+    //var
     public EmployerPage() {
         PageFactory.initElements(driver, this);
     }
+    //methods
 }
